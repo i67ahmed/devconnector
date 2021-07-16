4af33fd5b51e2713a69bdbc2a44610061d756641
i67ahmed <i67ahmed@gmail.com>
2021-07-16 17:46:01 -0400

    added user schema

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const user_schema = new schema ({
+  name : {
+    type : String,
+    required : true
+  },
+  email : {
+    type : String,
+    required : true
+  },
+  password : {
+    type : String,
+    required : true
+  },
+  avatar : {
+    type : String
+  },
+  date : {
+    type : Date,
+    default : Date.now
+  }
+});
