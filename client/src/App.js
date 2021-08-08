7b1fc28ed794047b42c3cea24b65bf3f30ab6aee
i67ahmed <i67ahmed@gmail.com>
2021-08-08 14:24:46 -0400

    error handling and redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
 import Footer from './components/layouts/footer';
 import Landing from './components/layouts/landing';
 import Navbar from './components/layouts/navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import store from './store';
 
 
 class App extends Component {
 
   render (){
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path = "/" component = {Landing}/>
-          <Route exact path = "/register" component = {Register}/>
-          <Route exact path = "/login" component={Login}/>
-          <Footer />
-        </div>
-      </Router>
+      <Provider store = {store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path = "/" component = {Landing}/>
+            <Route exact path = "/register" component = {Register}/>
+            <Route exact path = "/login" component={Login}/>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
   
