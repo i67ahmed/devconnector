18f7ca185e6220bfaabff061434bb794026f4b45
i67ahmed <i67ahmed@gmail.com>
2021-08-28 14:42:32 -0400

    components finished

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,42 +1,57 @@
-import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import { SET_ERRORS, SET_USER } from './types';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from '../utilis/setAuthHeader';
 
 //Register user
+
 export const registerUser = (userData,history) => dispatch => {
   axios
   .post('/api/users/register', userData)
-  .then(res => {
-    //Save the token to the local storage
-    const token = res.data.token;
-    localStorage.setItem('jwtToken', token);
-
-    //set the authheader
-    setAuthToken(token);
-
-    //Decode the token
-    const decoded = jwt_decode(token);
-    dispatch({
-      type: SET_CURRENT_USER,
-      payload: decoded
-    })
-  })
+  .then(res => history.push('/login'))
   .catch(err => 
     dispatch({
-      type: GET_ERRORS,
+      type: SET_ERRORS,
       payload: err.response.data
     })); 
 }
 
 //Login user
+
 export const loginUser = userData => dispatch => {
   axios
     .post('/api/users/login', userData)
-    .then(res => console.log(res.data))
+    .then((res) => {
+      //save the token to localstorage
+      const {token} = res.data;
+      localStorage.setItem('jwtToken', token);
+      //set token to auth header
+      setAuthToken(token);
+      //decode token
+      const decoded = jwt_decode(token);
+      //Write user info to redux
+      dispatch({
+        type: SET_USER,
+        payload: decoded,
+      });
+    })
     .catch(err =>
       dispatch({
-        type: GET_ERRORS,
+        type: SET_ERRORS,
         payload: err.response.data
       }));
+}
+
+//logout user
+
+export const logoutUser = () => dispatch => {
+  //Remove token from ls
+  localStorage.removeItem('jwtToken');
+  //Remove token from axios header
+  setAuthToken(false);
+  //Reset user in the redux store
+  dispatch({
+    type: SET_USER,
+    payload: {},
+  });
 }
